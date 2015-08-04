9c6db63d1481af42d68cce0302f9b86c9defa893
Luciano Delucchi <luciano.delucchi@gmail.com>
2015-08-04 17:30:19 +0200

    Implement basic functionality

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using Prism.Commands;
 using Prism.Mvvm;
+using SM.Model;
+using YASM.Services;
 
 namespace YASM.ViewModels
 {
 	public class MainWindowViewModel : BindableBase
 	{
-		public MainWindowViewModel()
+		readonly IServiceManager _serviceManager;
+		
+		public ObservableCollection<IService> Services { get; private set; }
+		
+		public ICommand TestCommand { get; set; }
+		
+		public IService SelectedService { get; set; }
+		
+		public MainWindowViewModel(IServiceManager serviceManager)
 		{
+			TestCommand = new DelegateCommand(Execute, CanExecute);
+			
+			Services = new ObservableCollection<IService>();
+			
+			this._serviceManager = serviceManager;
+			this._serviceManager.GetServices().ObserveOnDispatcher().Subscribe(Services.Add);
+		}
+		
+		void Execute()
+		{
+			var d = SelectedService.Description;
+		}
+		
+		bool CanExecute()
+		{
+			return true;
 		}
 	}
 }
